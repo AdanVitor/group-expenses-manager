e7413d011ac727819999017e5ec42d12f8a9c704
Adan Vitor <adanvitor90@gmail.com>
2014-11-14 17:12:25 -0200

    Set initial project

--- routes/groups.js
+++ routes/groups.js
@@ -1,13 +1,8 @@
 module.exports = function(app) {
-	var groups = app.controllers.groups
-	,autenticar = require("./../middlewares/autenticador");
+	var groups = app.controllers.groups;
+	var autenticar = require("./../middlewares/autenticador");
 	app.get("/groups", autenticar, groups.index);
 	app.get("/create_group",autenticar,groups.create_group);
 	app.post("/save_group",autenticar,groups.save_group);
-	app.get("/contact/:id", autenticar, groups.show);
-	app.post("/group", autenticar, groups.create);
-	app.get("/contact/:id/editar", autenticar, groups.edit);
-	app.put("/contact/:id", autenticar, groups.update);
-	app.delete("/contact/:id", autenticar, groups.destroy);
 	app.get("/test", autenticar, groups.get_group);
 };
