787cddc5e93b2e8e72e5186d62ca7cfe7292a33f
williampinho <william.comp.ita@gmail.com>
2014-11-15 15:54:41 -0200

    Create facebook canvas automatic login

---
+++ config/passport.js
@@ -0,0 +1,14 @@
+var FacebookStrategy = require('passport-facebook-canvas');
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
